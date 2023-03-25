c10c45af75831bac1a79599761b72016364af0a7
Kriti Shrestha <Kriti@Sarojs-MacBook-Pro.local>
2023-03-25 18:19:08 -0400

    Require inquirer and added array of questions

--- index.js
+++ index.js
@@ -1,10 +1,84 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require ('inquirer');
+const fs = require ('fs');
+const config = require ('./generateMarkdown.js')
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = [
+    {
+        type:'input',
+        message: "What is the title of your project",
+        name: 'title',
+    },
+    {type: 'input',
+    message: 'Please provide description of your project',
+    name: 'description',
+},
+{
+    type: 'input',
+    message: 'What are the table of contents',
+    name: 'contents',
+  },
+{
+    type: 'input',
+    message: 'What are the technologies Used?',
+    name: 'Technology',
+  },
+  
+  
+  {
+    type: 'Input',
+    message: 'Installation',
+    name: 'Installation'
+  },
+
+  {
+    type: 'Input',
+    message: 'Please provide usage of this application',
+    name:'Usage'
+  },
+  {
+    type: 'Input',
+    message: 'Please provide the details of contributors',
+    name:'contributors',
+    
+  },
+  {
+    type: 'checkbox',
+    message: 'License',
+    name:'license',
+    choices: ['MIT', 'GPL', 'Apache']
+  },
+  
+//   {
+//     type: "input",
+//     message: 'What is your github username?',
+//     name:'github',
+
+  
+//   {
+//     type: "list",
+//     message: 'What is your github userbame',
+//     name:'username',
+    
+//   },
+//   {
+//     type:'input',
+//     message: 'What is your email address?',
+//     name: 'email'
+//   }
+
+];
+inquirer.prompt(questions)
+.then((data)=>{
+
+})
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('Readme.md', data)
+}
 
 // TODO: Create a function to initialize app
 function init() {}
